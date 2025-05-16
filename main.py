217fd40c7d6e92e4f9155488259ae6cf9af53ced
nischit <nischitbaral5@gmail.com>
2025-05-16 15:01:45 +0545

    main file

--- main.py
+++ main.py
@@ -2,34 +2,28 @@ import logging
 import sys
 import argparse
 from etl import ETL
-from api import asc_data,desc_data
+
 
 def main():
     # zip_file = sys.argv[1]
 
     parser = argparse.ArgumentParser(description="ETL pipeline for processing ZIP files containing rate and provider data.")
     parser.add_argument("--zip_file", help="Path to the ZIP file to process")
-
-    parser.add_argument("--apiA",action='store_true', help="Path to the ZIP file to process")
-
-    parser.add_argument("--apiD",action='store_true', help="Path to the ZIP file to process")
-
+    parser.add_argument("--provider_dtl_path", help="Path to tht provider_detail.json")
+  
 
 
     args = parser.parse_args()
     
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO,filename='etl.log',datefmt='%Y-%m-%d %H:%M:%S')
 
     logger = logging.getLogger("ETL")
 
-    if args.apiA:
-        asc_data()
-    elif args.apiD:
-        desc_data()
+ 
         
-    if args.zip_file:
-        etl = ETL(logger)
-        etl.execute(args.zip_file)
+   
+    etl = ETL(logger)
+    etl.execute(args.zip_file,args.provider_dtl_path)
   
 
     # net_file,prov_path= extract_ano.ext_file(zip_file)
