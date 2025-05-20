14be369353d7b7f80381ed3b624ce44570ccf887
nischit <nischitbaral5@gmail.com>
2025-05-20 16:21:52 +0545

    etl_nrpr  file

---
+++ etl_nrpr.py
@@ -0,0 +1,37 @@
+
+import yaml
+from extract import extract_nrpr
+
+
+
+
+class ETL:
+
+        
+    def __init__(self,logger):
+        with open('new_file.yml', 'r') as file:
+          new_yml=  yaml.safe_load(file)  
+        
+        self.logger = logger
+        self.logger.info("Initializing ETL") 
+
+        self.cores = new_yml['SPARK']['EXECUTOR']
+        self.dri_mem = new_yml['SPARK']['DRIVER']['MEMORY']
+        self.exe_mem = new_yml['SPARK']['EXECUTOR']['MEMORY']
+        self.exe_core = new_yml['SPARK']['EXECUTOR']['CORES']
+        self.exe_instance = new_yml['SPARK']['EXECUTOR']['INSTANCES']
+
+
+        self.database = new_yml['POSTGRES']['DATABASE']
+        self.host = new_yml['POSTGRES']['HOST']
+        self.user = new_yml['POSTGRES']['USER']
+        self.password = new_yml['POSTGRES']['PASSWORD']
+        self.port = new_yml['POSTGRES']['PORT']
+
+
+
+    def execute(self,zip_filess):
+        
+       
+        self.logger.info("Extract")
+        extract_nrpr.ext_nrpr(zip_filess)
