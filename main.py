34644fce5f33e1b52c2713e0930afd22ad69b684
nischit <nischitbaral5@gmail.com>
2025-05-20 16:22:21 +0545

    main  file

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import logging
 import sys
 import argparse
-from etl import ETL
+from etl_nrpr import ETL
 
 
 def main():
     # zip_file = sys.argv[1]
 
     parser = argparse.ArgumentParser(description="ETL pipeline for processing ZIP files containing rate and provider data.")
-    parser.add_argument("--zip_file", help="Path to the ZIP file to process")
-    parser.add_argument("--provider_dtl_path", help="Path to tht provider_detail.json")
+    # parser.add_argument("--zip_file", help="Path to the ZIP file to process")
+    # parser.add_argument("--provider_dtl_path", help="Path to tht provider_detail.json")
+    parser.add_argument("--zip_filess", help="Path to the ZIP file to process")
+
   
 
 
@@ -23,9 +25,11 @@ def main():
         
    
     etl = ETL(logger)
-    etl.execute(args.zip_file,args.provider_dtl_path)
-  
+    # etl.execute(args.zip_file,args.provider_dtl_path)
+    etl.execute(args.zip_filess)
 
+  
+    
     # net_file,prov_path= extract_ano.ext_file(zip_file)
 
 
