ccaeca0689c68937df2ec2203b655a740e7d4647
nischit <nischitbaral5@gmail.com>
2025-05-21 13:56:40 +0545

    extract file

--- extract/extract_nrpr.py
+++ extract/extract_nrpr.py
@@ -3,39 +3,25 @@ import gzip
 import json
 import os
 
-
 def ext_nrpr(zip_filess):
-    
     output_folder = os.path.join(os.getcwd(), 'out_folder')
     os.makedirs(output_folder, exist_ok=True)
 
     combined_file = os.path.join(output_folder, 'combined_output.json')
-
-   
     all_data = []
 
-   
+    for filename in os.listdir(zip_filess):
+        if filename.endswith('.gz'):
+            gz_path = os.path.join(zip_filess, filename)
 
-    print("Extracting main ZIP file...")
-    with zipfile.ZipFile(zip_filess, 'r') as zip_ref:
-        zip_ref.extractall(output_folder)
-
-    for root, dirs, files in os.walk(output_folder):
-        for file in files:
-            if file.endswith('.gz'):
-                gz_path = os.path.join(root, file)
-                print("Reading:", gz_path)
-
-                with gzip.open(gz_path, 'rt', encoding='utf-8') as f:
-                    data = json.load(f)
-                    all_data.append(data)
+            with gzip.open(gz_path, 'rt', encoding='utf-8') as f:
+                data = json.load(f)
+                all_data.append(data)
 
     with open(combined_file, 'w', encoding='utf-8') as out_file:
         json.dump(all_data, out_file, indent=2)
+        out_file.write('\n')
 
     return combined_file
-
-
-
 
 
