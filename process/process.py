b35e6b5aff2f6a72d4fd85d00e0be7a7b7794eb4
nischit <nischitbaral5@gmail.com>
2025-05-23 16:34:47 +0545

    process file

--- process/process.py
+++ process/process.py
@@ -1,18 +1,23 @@
 
-def process_file(main_provider,df_arr4,rate_tbl,df_select,etl,logger):
+from pyspark.sql.functions import size,array_intersect,col
+
+def process_file(main_provider,df_arr4,rate_tbl,df_select):
 
     df_join = main_provider.join(df_arr4,how="inner",on=["npi","tin"])
-    df_newjin = rate_tbl.join(df_select,how="inner",on=["billing_code"] )
+    df_newjin = rate_tbl.join(df_select,how="inner",on="billing_code" )
     df_newjin.printSchema()
 
 
     df_joined = df_newjin.join( df_join,how="inner",on='provider_group_id')
 
+    filter_spec = df_joined.filter(size(array_intersect(col("prv_taxonomy"), col("taxonomy_list"))) > 0)
+
+    filter_spec.show()
 
     net_nrpr = 'out_folder/net_nrpr.parquet'
-    prv_nrpr = 'out_folder/net_nrpr.parquet'
+    prv_nrpr = 'out_folder/prv_nrpr.parquet'
 
     df_newjin.write.mode('overwrite').parquet(net_nrpr)
-    df_join.write.mode('overwrite').parquet(prv_nrpr)
+    df_join.write.mode('overwrite').parquet(prv_nrpr)   
 
     return net_nrpr,prv_nrpr
